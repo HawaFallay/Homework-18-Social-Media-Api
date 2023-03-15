617f5ee8758dccf80acb364140638713551e07ba
HawaFallay <113000340+HawaFallay@users.noreply.github.com>
2023-03-15 16:29:17 -0700

    all routes complete

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+    getThoughts,
+    createThoughts,
+    updateThought,
+    deleteThought,
+    getSingleThought,
+    createReaction,
+    deleteReaction,
+
+} = require('../../controllers/thoughtController');
+
+router.route('/').get(getThoughts).post(createThoughts);
+router.route('/:thoughtId')
+.put(updateThought).delete(deleteThought).get(getSingleThought);
+
+router.route('/:thoughtId/reactions').post(createReaction);
+router.route('/:thoughtId?reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
