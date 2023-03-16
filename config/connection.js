40e94e4bbad6b0216411d3eeb4858bf0022f8d71
HawaFallay <113000340+HawaFallay@users.noreply.github.com>
2023-03-15 17:21:40 -0700

    mongodb

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+mongoose.connect('mongodb://127.0.0.1:27017/homework18');
 
-const connectionString = process.env.MONGODB_URI || 'mongodb:'
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/homework18'
 
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
