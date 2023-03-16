d8bd93fc3835a6928a307679937e8633c7d0b4b9
HawaFallay <113000340+HawaFallay@users.noreply.github.com>
2023-03-15 17:05:22 -0700

    edits to js files

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/test');
 
 const connectionString = process.env.MONGODB_URI || 'mongodb:'
 
---
+++ models/thought.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+const reactionSchema = require('./reaction-schema');
+const moment = require('moment');
